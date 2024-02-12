22290e976f27ee34ab40a75314c720402f6d71e4
Abiud Sokomi <abiudsokomi@gmail.com>
2024-02-12 12:26:14 +0300

    my commit

---
+++ assignment.java
@@ -0,0 +1,91 @@
+class Assignment
+{
+    public static void main(String[] args) {
+        //Addition
+        int num1 = 3;
+        int num2 = 4;
+
+        int result = num1 + num2;
+
+        //subtraction
+        int num3 = 3;
+        int num4 = 4;
+
+        int result2 = num3 - num4;
+
+        //division
+        int num5 = 3;
+        int num6 = 4;
+
+        int result3 = num5 / num6;
+
+        //remainder in division
+        int num7 = 7;
+        int num8 = 4;
+
+        int result4 = num7 % num8;
+
+        //pre-increment
+        int num9 = 10;
+        ++num9;
+
+        int num10 = 10;
+        int result5 = ++num10;//the result will first increment=11
+
+        //post-increment
+        int num11 = 10;
+        num11++;
+
+        int num12 = 10;
+        int result6 = num12++;//value appears before incrementing=10
+
+        //decrement
+        int inc = 4;
+        --inc;
+
+        int four = 4;
+        four--;
+
+        int five = 5;
+        int res = --five;
+        //System.out.println(res);
+
+        //System.out.println(four);
+//System.out.println(inc);
+
+// int six = 6;
+// int output = six--;
+// System.out.println(six);
+
+        //short-hand
+       int  num13 = 14;
+       num13+=3;
+
+       int  num14 = 14;
+       num14-=3;
+
+       int  num15 = 14;
+       num15%=3;
+
+       int  num16 = 14;
+       num16/=3;
+
+       int  num17 = 14;
+       num17*=3;
+
+
+        // System.out.println(result);
+        // System.out.println(result2);
+        // System.out.println(result3);
+        // System.out.println(result4);
+        // System.out.println(num9);
+        // System.out.println(result5);
+        System.out.println(num11);
+        System.out.println(result6);
+        // System.out.println(num13);
+        // System.out.println(num14);
+        // System.out.println(num15);
+        // System.out.println(num16);
+        // System.out.println(num17);
+    }
+}
